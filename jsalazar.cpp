2ceb04c8fef2c16221af6ddc564478d03c7b6bad
Jsalazar68 <jsalazar@odin.cs.csubak.edu>
2024-10-22 08:33:56 -0700

    Worked on Bomb class

---
+++ jsalazar.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "sslone.h"
+#include "jsalazar.h"
+   /*
+   string Bomb::getWeapon()
+   {
+       return this->type;
+   }
+   
+  Bomb::Bomb(int in_rate, Player * in_parent, PhysWorld * in_member) : Weapon(
+  
+  Bomb::~Bomb(){}
+  
+  
+  //void Bomb::fireWeapon{}
+  void Bomb::physics(){}
+  
+  
+  
+  
+  void Bomb::render()
+  {
+  
+  
+  }
+*/
--- jsalazar.h
+++ jsalazar.h
@@ -1,1 +1,21 @@
-// code here
+ /*class Bomb: public Weapon
+   {
+       private:
+       Vec pos;
+       Vec vel;
+       float color[3];
+       Bullet * barr;
+      struct timespec bulletTimer;
+      int nbullets;
+      //const char * type = "Bomb";
+     // std::string type = "Bomb";
+      public:
+      PhysWorld * member;
+      Bomb(int in_rate, Player * in_parent, PhysWorld * in_member);
+      ~Bomb();
+      std::string getWeapon();
+      void fireWeapon();
+      void physics();
+      void render();
+  
+  };*/      
